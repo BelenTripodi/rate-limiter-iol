80474cb7cde067ddc4b74e09503f90c48410c178
Maria Belen Tripodi <maria.tripodi@despegar.com>
2026-02-17 22:56:58 -0300

    final commit

--- src/main/kotlin/com/example/ratelimiter/web/controller/Controller.kt
+++ src/main/kotlin/com/example/ratelimiter/web/controller/Controller.kt
@@ -2,11 +2,16 @@ package com.example.ratelimiter.web.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping
-class HelloController {
+class Controller {
   @GetMapping("/hello")
   fun hello(): Map<String, String> = mapOf("message" to "hello")
+
+  @GetMapping("/v1/search")
+  fun search(@RequestParam("q", required = false) query: String?): Map<String?, String> =
+      mapOf(query to "hello")
 }
